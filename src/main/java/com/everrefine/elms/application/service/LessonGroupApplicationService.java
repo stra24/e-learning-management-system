e7138916195f71a08d7723e24daf3abee4c3170f
fumiya11 <shimanekennow@gmail.com>
2025-11-18 16:59:23 +0900

    レッスングループのアプリケーションサービスの実装

---
+++ src/main/java/com/everrefine/elms/application/service/LessonGroupApplicationService.java
@@ -0,0 +1,9 @@
+package com.everrefine.elms.application.service;
+
+import com.everrefine.elms.application.command.LessonGroupCreateCommand;
+import com.everrefine.elms.application.dto.LessonGroupDto;
+
+public interface LessonGroupApplicationService {
+
+  LessonGroupDto createLessonGroup(LessonGroupCreateCommand lessonGroupCreateCommand);
+}
---
+++ src/main/java/com/everrefine/elms/application/service/LessonGroupApplicationServiceImpl.java
@@ -0,0 +1,45 @@
+package com.everrefine.elms.application.service;
+
+import com.everrefine.elms.application.command.LessonGroupCreateCommand;
+import com.everrefine.elms.application.dto.LessonGroupDto;
+import com.everrefine.elms.domain.model.lesson.LessonGroup;
+import com.everrefine.elms.domain.repository.LessonGroupRepository;
+import com.everrefine.elms.domain.service.LessonGroupDomainService;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class LessonGroupApplicationServiceImpl implements LessonGroupApplicationService {
+
+  private final LessonGroupRepository lessonGroupRepository;
+  private final LessonGroupDomainService lessonGroupDomainService;
+
+  @Override
+  @Transactional
+  public LessonGroupDto createLessonGroup(LessonGroupCreateCommand lessonGroupCreateCommand) {
+    // レッスンの並び順を自動発番
+    BigDecimal lessonGroupOrder = lessonGroupDomainService.issueLessonGroupOrder(
+        lessonGroupCreateCommand.getCourseId());
+
+    LessonGroup lessonGroup = LessonGroup.create(
+        lessonGroupCreateCommand.getCourseId(),
+        lessonGroupOrder,
+        lessonGroupCreateCommand.getTitle()
+    );
+
+    LessonGroup createdLessonGroup = lessonGroupRepository.createLessonGroup(lessonGroup);
+
+    return new LessonGroupDto(
+        createdLessonGroup.getId(),
+        createdLessonGroup.getCourseId(),
+        createdLessonGroup.getLessonGroupOrder().getValue(),
+        createdLessonGroup.getTitle().getValue(),
+        createdLessonGroup.getCreatedAt(),
+        createdLessonGroup.getUpdatedAt(),
+        null
+    );
+  }
+}
