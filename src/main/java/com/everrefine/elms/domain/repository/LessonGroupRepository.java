b51de6acdfca5e145d961605a2420a00752bcb29
fumiya11 <shimanekennow@gmail.com>
2025-11-18 16:58:50 +0900

    レッスングループのレポジトリ実装

---
+++ src/main/java/com/everrefine/elms/domain/repository/LessonGroupRepository.java
@@ -0,0 +1,12 @@
+package com.everrefine.elms.domain.repository;
+
+import com.everrefine.elms.domain.model.lesson.LessonGroup;
+import java.math.BigDecimal;
+import java.util.Optional;
+
+public interface LessonGroupRepository {
+
+  LessonGroup createLessonGroup(LessonGroup lessonGroup);
+
+  Optional<BigDecimal> findMaxLessonGroupOrderByCourseId(Integer courseId);
+}
