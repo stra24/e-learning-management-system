3086cd5b5eb7079890993f6da6a0d8bcb9fa6106
Toshiaki Mitsukura <mitsukura@gmail.com>
2025-12-16 23:15:03 +0900

    refactor: updateLessonGroup を削除して責務を整理

--- src/main/java/com/everrefine/elms/domain/service/LessonGroupDomainService.java
+++ src/main/java/com/everrefine/elms/domain/service/LessonGroupDomainService.java
@@ -1,6 +1,5 @@
 package com.everrefine.elms.domain.service;
 
-import com.everrefine.elms.domain.model.lesson.LessonGroup;
 import java.math.BigDecimal;
 
 public interface LessonGroupDomainService {
@@ -12,13 +11,4 @@ public interface LessonGroupDomainService {
    * @return 発番されたレッスングループの並び順
    */
   BigDecimal issueLessonGroupOrder(Integer courseId);
-
-  /**
-   * レッスングループを更新する。
-   *
-   * @param lessonGroup 更新前のレッスングループ
-   * @param title       新しいタイトル
-   * @return 更新後のレッスングループ
-   */
-  LessonGroup updateLessonGroup(LessonGroup lessonGroup, String title);
 }
